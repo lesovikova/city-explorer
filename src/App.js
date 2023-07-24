20c0e67bcf209455de4fcdfc01d58980e5416cab
__lesovikova__ <__lesovikova@gmail.com__>
2023-07-24 15:13:50 +0100

    add map

--- src/App.js
+++ src/App.js
@@ -14,15 +14,22 @@ function App() {
       e.preventDefault();
       e.target.input.value = "";
       const API = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=${cityName}&format=json`;
-      console.log(API);
       const res = await axios.get(API);
       console.log(res);
       setCity(res.data[0]);
+      displayMap(res.data[0]);
       
       }
       catch (error) {
         console.log(error);
       }
+    }
+
+    const [map, setMap] = useState('');
+    function displayMap(city) {
+      const API = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${city.lat},${city.lon}&zoom=10`;
+      setMap(API);
+      console.log(map);
     }
 
   return (
@@ -32,6 +39,8 @@ function App() {
         <button>Explore!</button>
       </form>
       <p>You requested: {cityName}, the location is {city.display_name}, its longitude is {city.lon}, latitude - {city.lat}</p>
+      {console.log(map)}
+      {map && <img src={map} alt="map"/>}
     </div>
   );
 }
