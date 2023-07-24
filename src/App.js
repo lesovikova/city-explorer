afcf889d9bca68b3c3038bc3cc8d9087a50487c7
__lesovikova__ <__lesovikova@gmail.com__>
2023-07-24 14:44:29 +0100

    add town search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import axios from "axios";
 
 function App() {
+    const [cityName, setCityName] = useState("");
+    function getSearchWord(e) {
+      setCityName(e.target.value);
+    }
+
+    const [city, setCity] = useState({});
+    async function getCity(e){
+      try {
+      e.preventDefault();
+      e.target.input.value = "";
+      const API = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=${cityName}&format=json`;
+      console.log(API);
+      const res = await axios.get(API);
+      console.log(res);
+      setCity(res.data[0]);
+      
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getCity}>
+        <input onChange={getSearchWord} type='text' placeholder='enter a city name' name='input' />   
+        <button>Explore!</button>
+      </form>
+      <p>You requested: {cityName}, the location is {city.display_name}, its longitude is {city.lon}, latitude - {city.lat}</p>
     </div>
   );
 }
